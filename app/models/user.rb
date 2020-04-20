d326ddca16c714820292a27a27399838b752c147
JosefinaAcevedo <jacevedo@miuandes.cl>
2020-04-20 11:05:24 -0400

    add changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :events
   has_many :event_votes
   has_many :organization_users
+  has_many :event_guests
   #has_one :organization , optional:true #only if its an admin
 
   validates :username, :email, presence: true
