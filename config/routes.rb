ea3e26fb58e483b3da2e07438a283acd22400ae2
Magdalena Barrett <mibarrett@miuandes.cl>
2020-05-07 11:21:07 -0400

    agregue api y v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events do
-    resources :comments, shallow: true
+  root "pages#home"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :events do
+        resources :comments, shallow: true
+      end
+    end
   end
+
+  resources :comments, defaults: { format: :html }
+  resources :events, defaults: { format: :html}
+
 end
