afacc4d123156986749ddfea6f009ada7f8eb10d
JosefinaAcevedo <jacevedo@miuandes.cl>
2020-05-10 23:01:06 -0400

    replies

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     #@organization = Organization.where(events: @event)
     @event_comments = @event.comments
-    @organization = @event.organizations
+    @organization = @event.organization
   end
 
   # GET /events/new
